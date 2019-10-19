a2957950d192ce4dcacd4bc38a835999989f85c4
Ulises Santana <ulisesantana@gmail.com>
2019-10-19 11:04:19 +0100

    hackaton code

---
+++ docs/styles-ae0c0ac0a12fc6468f0d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{353:function(n,w,o){}}]);
+//# sourceMappingURL=styles-ae0c0ac0a12fc6468f0d.js.map
---
+++ src/store/data.ts
@@ -0,0 +1,13 @@
+import {Store} from "../types";
+
+export const store: Store = {
+  assets: {},
+  holders: {},
+  records: []
+} as Store;
+
+export const storeMock: Store = {
+  assets: {},
+  holders: {},
+  records: []
+};
