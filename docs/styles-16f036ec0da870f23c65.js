b3aeaecac821dab3f75d440f8c3e59fc8083ae96
Ulises Santana <ulisesantana@gmail.com>
2019-10-23 17:34:00 +0100

    :pencil: udpate docs

--- docs/styles-16f036ec0da870f23c65.js
+++ docs/styles-16f036ec0da870f23c65.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[7],{353:function(n,w,o){}}]);
-//# sourceMappingURL=styles-43b9d73a91d883e29b7b.js.map
+//# sourceMappingURL=styles-16f036ec0da870f23c65.js.map
